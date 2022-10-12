192eb802e0e569d4fefae4b6395f6c9d1b88e477
Anders Gustafsson <andersgee@gmail.com>
2022-10-12 08:19:22 +0200

    tidy sign-in page

--- src/icons/Person.tsx
+++ src/icons/Person.tsx
@@ -1,12 +1,8 @@
-import { SVGProps } from "react";
+import type { IconProps } from "./index";
 
-type Props = {
-  className?: string;
-} & SVGProps<SVGSVGElement>;
-
-export function Person({ className, ...rest }: Props) {
+export function Person({ className, width = 48, height = 48, ...rest }: IconProps) {
   return (
-    <svg className={className} width="48" height="48" viewBox="0 0 24 24" {...rest}>
+    <svg viewBox="0 0 24 24" className={className} width={width} height={height} {...rest}>
       <circle cx="12" cy="8" r="4" />
       <path d="M12 12 m0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
     </svg>
---
+++ src/icons/index.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export type IconProps = {
+  className?: string;
+  width?: number;
+  height?: number;
+} & SVGProps<SVGSVGElement>;
